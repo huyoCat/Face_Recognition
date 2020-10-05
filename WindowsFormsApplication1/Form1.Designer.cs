2f67bf7c91c8e3828876462509e559fbc33e3e90
huyoCat <yexiu0127@gmail.com>
2020-10-05 13:45:30 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.btNext = new System.Windows.Forms.Button();
+            this.btRotate90 = new System.Windows.Forms.Button();
+            this.btRotate180 = new System.Windows.Forms.Button();
+            this.btRec = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.CutPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CutPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(625, 386);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btOpen
+            // 
+            this.btOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btOpen.Location = new System.Drawing.Point(747, 29);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(75, 23);
+            this.btOpen.TabIndex = 1;
+            this.btOpen.Text = "打开图片";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // btNext
+            // 
+            this.btNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btNext.Location = new System.Drawing.Point(747, 86);
+            this.btNext.Name = "btNext";
+            this.btNext.Size = new System.Drawing.Size(75, 23);
+            this.btNext.TabIndex = 2;
+            this.btNext.Text = "下一张";
+            this.btNext.UseVisualStyleBackColor = true;
+            this.btNext.Click += new System.EventHandler(this.btNext_Click);
+            // 
+            // btRotate90
+            // 
+            this.btRotate90.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btRotate90.Location = new System.Drawing.Point(657, 29);
+            this.btRotate90.Name = "btRotate90";
+            this.btRotate90.Size = new System.Drawing.Size(75, 23);
+            this.btRotate90.TabIndex = 3;
+            this.btRotate90.Text = "旋转90";
+            this.btRotate90.UseVisualStyleBackColor = true;
+            this.btRotate90.Click += new System.EventHandler(this.btRotate90_Click);
+            // 
+            // btRotate180
+            // 
+            this.btRotate180.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btRotate180.Location = new System.Drawing.Point(657, 86);
+            this.btRotate180.Name = "btRotate180";
+            this.btRotate180.Size = new System.Drawing.Size(75, 23);
+            this.btRotate180.TabIndex = 4;
+            this.btRotate180.Text = "旋转180";
+            this.btRotate180.UseVisualStyleBackColor = true;
+            this.btRotate180.Click += new System.EventHandler(this.btRotate180_Click);
+            // 
+            // btRec
+            // 
+            this.btRec.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btRec.Location = new System.Drawing.Point(657, 151);
+            this.btRec.Name = "btRec";
+            this.btRec.Size = new System.Drawing.Size(75, 23);
+            this.btRec.TabIndex = 5;
+            this.btRec.Text = "人脸识别";
+            this.btRec.UseVisualStyleBackColor = true;
+            this.btRec.Click += new System.EventHandler(this.btRec_Click);
+            // 
+            // btSave
+            // 
+            this.btSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btSave.Location = new System.Drawing.Point(747, 151);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 6;
+            this.btSave.Text = "保存";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // CutPicture
+            // 
+            this.CutPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CutPicture.Location = new System.Drawing.Point(657, 214);
+            this.CutPicture.Name = "CutPicture";
+            this.CutPicture.Size = new System.Drawing.Size(165, 156);
+            this.CutPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.CutPicture.TabIndex = 7;
+            this.CutPicture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 483);
+            this.Controls.Add(this.CutPicture);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.btRec);
+            this.Controls.Add(this.btRotate180);
+            this.Controls.Add(this.btRotate90);
+            this.Controls.Add(this.btNext);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "图片查看器";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CutPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.Button btNext;
+        private System.Windows.Forms.Button btRotate90;
+        private System.Windows.Forms.Button btRotate180;
+        private System.Windows.Forms.Button btRec;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.PictureBox CutPicture;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using System.Drawing.Imaging;
+using Baidu.Aip.Face;
+using System.Net;
+using System.Web;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms.VisualStyles;
+using VisioForge.Shared.MediaFoundation.OPM;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private int ImageCount;
+        private List<string> ImagePaths = new List<string>();
+        private int nowCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (DialogResult.OK == openFileDialog1.ShowDialog())
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                    foreach (string Path in Directory.GetFiles(
+                        Path.GetDirectoryName(openFileDialog1.FileName)))
+                    {
+                        ImagePaths.Add(Path);
+                    }
+                    if (ImagePaths.Count != 0)
+                    {
+                        ImageCount = ImagePaths.Count;
+                    }
+                }
+            }
+            catch(OutOfMemoryException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btNext_Click(object sender, EventArgs e)
+        {
+
+            if (nowCount < ImageCount)
+            {
+                this.pictureBox1.Image = Bitmap.FromFile(ImagePaths[nowCount]);
+                nowCount++;
+                this.CutPicture.Hide();
+            }
+
+        }
+
+        private void btRotate90_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            this.pictureBox1.Refresh();
+        }
+
+        private void btRotate180_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            this.pictureBox1.Refresh();
+        }
+
+        private void btRec_Click(object sender, EventArgs e)
+        {
+            Painting();
+        }
+        public string ReadImg(string img)
+        {
+            return Convert.ToBase64String(File.ReadAllBytes(img));
+        }
+
+        /// <summary>
+        /// 画框
+        /// </summary>
+        public void Painting()
+        {
+            //this.pictureBox1.Image = Bitmap.FromFile(ImagePaths[nowCount]);
+            Bitmap bitMap = new Bitmap(pictureBox1.Image);
+            // 设置APPID/AK/SK
+            var API_KEY = "3USlfs0IEY92KNpbeedm1lp7";
+            var SECRET_KEY = "YpNKGlZ1SHeIFHIf6RHUvokHDetpnc9F";
+            var client = new Face(API_KEY, SECRET_KEY);
+            client.Timeout = 60000;  // 修改超时时间
+            client.AppId = "22778055";
+            string guidName = Guid.NewGuid() + ".Jpeg";
+            bitMap.Save(guidName, ImageFormat.Jpeg);
+            var image = ReadImg(guidName);
+            var imageType = "BASE64";
+            var options = new Dictionary<string, object>{
+                {"face_field","face_shape"},
+                {"max_face_num", 5}};
+            try
+            {
+                var result = client.Detect(image, imageType, options);
+                RootObject list = JsonConvert.DeserializeObject<RootObject>(result.ToString());
+                Graphics gobj = pictureBox1.CreateGraphics();
+                Pen redPen = new Pen(Color.Red, 1);
+                for (int i = 0; i < (int)double.Parse(list.result.face_num); i++)
+                {
+                    Double a = double.Parse(list.result.face_list[i].location.left);
+                    Double b = double.Parse(list.result.face_list[i].location.top);
+                    Double c = double.Parse(list.result.face_list[i].location.width);
+                    Double d = double.Parse(list.result.face_list[i].location.height);
+                    Rectangle myRectangle = new Rectangle((int)a, (int)b, (int)c, (int)d);
+                    gobj.DrawRectangle(redPen, myRectangle);
+
+                    this.CutPicture.Show();
+                    CutPicture.Image = CutPic(pictureBox1.Image, (int)a, (int)b, (int)c, (int)d);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("未检测到人脸");
+            }
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Image = Bitmap.FromFile(ImagePaths[nowCount]);
+            Bitmap bitMap = new Bitmap(pictureBox1.Image);
+            //CutPic(bitMap);
+            // 设置APPID/AK/SK
+            var API_KEY = "3USlfs0IEY92KNpbeedm1lp7";
+            var SECRET_KEY = "YpNKGlZ1SHeIFHIf6RHUvokHDetpnc9F";
+            var client = new Face(API_KEY, SECRET_KEY);
+            client.Timeout = 60000;  // 修改超时时间
+            client.AppId = "22778055";
+            string guidName = Guid.NewGuid() + ".Jpeg";
+            bitMap.Save(guidName, ImageFormat.Jpeg);
+            var image = ReadImg(guidName);
+            var imageType = "BASE64";
+            var options = new Dictionary<string, object>{
+                {"face_field","face_shape"},
+                {"max_face_num", 5}};
+            var result = client.Detect(image, imageType, options);
+            RootObject list = JsonConvert.DeserializeObject<RootObject>(result.ToString());
+            for (int i = 0; i < (int)double.Parse(list.result.face_num); i++)
+            {
+                Double a = double.Parse(list.result.face_list[i].location.left);
+                Double b = double.Parse(list.result.face_list[i].location.top);
+                Double c = double.Parse(list.result.face_list[i].location.width);
+                Double d = double.Parse(list.result.face_list[i].location.height);
+                CutPicture.Image = CutPic(pictureBox1.Image, (int)a, (int)b, (int)c, (int)d);
+                DialogResult dr = MessageBox.Show("确定保存人脸？", "保存提示", MessageBoxButtons.YesNo, 
+                    MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    SaveFileDialog saveImageName = new SaveFileDialog();
+
+                    if (saveImageName.ShowDialog() == DialogResult.OK)
+                    {
+                        CutPicture.Image.Save(saveImageName.FileName);
+                    }
+                }
+            }
+            bitMap.Dispose();
+
+        }
+
+        public Image CutPic(Image img,int a,int b,int c,int d)
+        {
+            Bitmap pic = new Bitmap(img);
+            Bitmap resultBitmap = new Bitmap(c, d);
+            using (Graphics g = Graphics.FromImage(resultBitmap))
+            {
+                Rectangle resultRectangle = new Rectangle(0, 0, c, d);
+                Rectangle sourceRectangle = new Rectangle(a, b, c, d);
+                g.DrawImage(pic, resultRectangle, sourceRectangle, GraphicsUnit.Pixel);
+            }
+            pic.Dispose();
+            return resultBitmap;
+        }      
+
+        public class Location
+        {
+            public string left { get; set; }
+            public string top { get; set; }
+            public string width { get; set; }
+            public string height { get; set; }
+            public string rotation { get; set; }
+        }
+
+        public class Angle
+        {
+            public string yaw { get; set; }
+            public string pitch { get; set; }
+            public string roll { get; set; }
+        }
+
+        public class Landmark
+        {
+            public string x { get; set; }
+            public string y { get; set; }
+        }
+
+        public class Landmark72
+        {
+            public string x { get; set; }
+            public string y { get; set; }
+        }
+
+        public class Face_list
+        {
+            public string face_token { get; set; }
+            public Location location { get; set; }
+            public string face_probability { get; set; }
+            public Angle angle { get; set; }
+            public List<Landmark> landmark { get; set; }
+            public List<Landmark72> landmark72 { get; set; }
+        }
+
+        public class Result
+        {
+            public string face_num { get; set; }
+            public List<Face_list> face_list { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string error_code { get; set; }
+            public string error_msg { get; set; }
+            public string log_id { get; set; }
+            public string timestamp { get; set; }
+            public string cached { get; set; }
+            public Result result { get; set; }
+        }
+
+    }
+}
